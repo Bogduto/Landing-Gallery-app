00b242021865cf3311cc2ae84324ed14a3389f05
bogdan <bogdanbogdanvakulenka@gmail.com>
2023-10-28 19:20:18 +0300

    remove project with image route

--- app/api/private-api/project/management/remove/[id]/route.js
+++ app/api/private-api/project/management/remove/[id]/route.js
@@ -2,32 +2,30 @@ import { NextResponse } from "next/server"
 import connectMongodb from "@/connects/mongoose"
 import cartModel from "@/schemas/CartModel"
 import { unlink } from "fs";
-
 export async function DELETE(req, {
     params
 }) {
     try {
         const id = params.id
-        const body = await req.json()
-
         await connectMongodb()
 
         const cart = await cartModel
-        const findAndUpdateCart = await cart.deleteOne({ _id: body._id })
+        const findAndUpdateCart = await cart.findByIdAndRemove({ _id: id })
 
-        unlink(`./public/uploads/${body.img}`, (error) => {
+        if (!findAndUpdateCart) console.error("not found or data is null")
+
+        const imageName = findAndUpdateCart.screenshot
+        unlink(`./public/uploads/${imageName}`, (error) => {
             if (error) {
                 throw new Error(error);
             }
         });
-
 
         return NextResponse.json({
             msg: "Project removed",
         }, {
             status: 410
         });
-
     } catch (error) {
         throw new Error(error)
     }
--- app/api/test/removeImg/route.js
+++ app/api/test/removeImg/route.js
@@ -1,21 +1,33 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
+import connectMongodb from "@/connects/mongoose"
+import cartModel from "@/schemas/CartModel"
 import { unlink } from "fs";
-export async function DELETE(req, res) {
-    try {
-        const body = await req.json()
 
-        unlink(`./public/uploads/${body.img}`, (error) => {
+export async function POST(req, {
+    params
+}) {
+    try {
+    
+
+        await connectMongodb()
+
+        const cart = await cartModel
+        const findAndUpdateCart = await cart.findOneAndRemove({ _id: body._id })
+
+        const imageName = findAndUpdateCart.screenshot
+
+        unlink(`./public/uploads/${imageName}`, (error) => {
             if (error) {
                 throw new Error(error);
             }
         });
 
         return NextResponse.json({
-            msg: "removed",
+            msg: "Project removed",
         }, {
             status: 410
         });
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
-}
+} 
