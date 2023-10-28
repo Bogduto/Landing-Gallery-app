e8e63950a10871b1029283fed6f8cc9787b12cd6
bogdan <bogdanbogdanvakulenka@gmail.com>
2023-10-28 12:24:37 +0300

    remove max length of description from form. normal transform in forms

--- validations/YapValidator.js
+++ validations/YapValidator.js
@@ -3,7 +3,6 @@ import * as Yup from "yup";
 export const createNewProjectValidator = Yup.object().shape({
   description: Yup.string()
     .min(40, "Too Short!")
-    .max(500, "Too Long!")
     .required("deskription is Required"),
   email: Yup.string().email("Invalid email").required("email is required"),
   websiteUrl: Yup.string().required("websiteUrl is required"),
