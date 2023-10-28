7f995d4c45d44e3c3274bf78d86d93f29334117c
bogdan <bogdanbogdanvakulenka@gmail.com>
2023-10-28 21:17:35 +0300

    fds

--- app/api/private-api/project/getAll/route.js
+++ app/api/private-api/project/getAll/route.js
@@ -9,7 +9,7 @@ export async function GET(req, res) {
 
         const cart = await cartModel
 
-        const findAllCarts = await cart.find({ verified: false }).exec()
+        const findAllCarts = await cart.find({})
 
         return NextResponse.json({
             msg: "get all carts",
--- app/api/public-api/project/getAll/route.js
+++ app/api/public-api/project/getAll/route.js
@@ -9,7 +9,7 @@ export async function GET(req, res) {
 
         const cart = await cartModel
 
-        const findAllCarts = await cart.find({ verified: true }).exec()
+        const findAllCarts = await cart.find({})
 
         return NextResponse.json({
             msg: "get all carts",
