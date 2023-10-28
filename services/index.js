3cf187841ef5cdf1162e6615e9971d3ec05f0c96
bogdan <bogdanbogdanvakulenka@gmail.com>
2023-10-28 21:08:55 +0300

    fix url paths

--- services/index.js
+++ services/index.js
@@ -28,7 +28,7 @@ export const createOneProject = async (values) => {
 
 export const getAllProjectsClientByCategory = async (category) => {
     try {
-        const path = category ? `${url}/public-api/project/getAllByCategory/${category}/` : `${url}/public-api/project/getAll`
+        const path = category ? `${url}/public-api/project/getAllByCategory/${category}/` : `${url}/public-api/project/getAll/`
         
         const carts = await fetch(path, {
             method: "GET",
@@ -45,7 +45,7 @@ export const getAllProjectsClientByCategory = async (category) => {
 
 export const getAllProjectsClient = async () => {
     try {
-        const carts = await fetch(`${url}/public-api/project/getAll`, {
+        const carts = await fetch(`${url}/public-api/project/getAll/`, {
             method: "GET",
             cache: "no-cache",
         })
