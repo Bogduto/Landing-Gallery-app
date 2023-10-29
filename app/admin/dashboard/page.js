04a3e51ea007c23aaf9486cff2fa6ad99356df1a
bogdan <bogdanbogdanvakulenka@gmail.com>
2023-10-29 11:27:44 +0200

    dynamic

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -4,6 +4,10 @@ import { getAllProjectsAdmin } from '@/services';
 // components
 import Carts from './components/carts'
 
+export const dynamic = 'force-dynamic'
+
+export const revalidate = 0
+
 const AdminDashboard = async () => {
     const data = await getAllProjectsAdmin();
     console.log(data)
